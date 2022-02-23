bf95ce8bc486cbfa9060a60d8e89208a30f4742c
naychi1996 <85649841+naychi1996@users.noreply.github.com>
2022-02-23 09:46:12 -0800

    Add files via upload

---
+++ game.js
@@ -0,0 +1,75 @@
+buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+$(document).keydown(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function nextSequence() {
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  makeSound(randomChosenColor);
+}
+
+$(".btn").click(function handlerFunction() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  makeSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function makeSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  var activeButton = $("#" + currentColor);
+  activeButton.addClass("pressed");
+  setTimeout(function() {
+    activeButton.removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel){
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+  if(userClickedPattern.length === gamePattern.length) {
+    setTimeout(function() {
+      nextSequence();} ,1000);
+  }}
+  else
+  {
+    console.log("wrong");
+    makeSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Level "+level+": Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  gamePattern = [];
+  level = 0;
+  started = false;
+}
